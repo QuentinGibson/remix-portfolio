0d5556dfa076b7164bb699f9b94d944da821d34b
Quentin Gibson <quentingibson94@gmail.com>
2023-05-11 01:30:16 -0400

    add some content

---
+++ app/routes/_index.tsx
@@ -0,0 +1,9 @@
+export default function IndexRoute() {
+  return (
+    <main className="bg-orange-500 relative h-full px-4 py-4">
+      <section className="bg-white flex flex-col justify-around items-center h-full">
+        <h1>Hello World!</h1>
+      </section>
+    </main>
+  );
+};
