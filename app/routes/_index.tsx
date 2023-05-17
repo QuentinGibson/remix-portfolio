2721a69ffe8f6e17e9a2ea2ff048e27b54d871f8
Quentin Gibson <quentingibson94@gmail.com>
2023-05-17 02:50:06 -0400

    index page check

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,46 +7,50 @@ import { Link } from '@remix-run/react';
 export default function IndexRoute() {
   return (
     <main className="relative h-full">
-      <section className='py-8 bg-cream'>
-        <div className='relative flex justify-center'>
-          <img src="/img/nobg-me.png" className='object-fill w-full z-20' alt="This is some random ass nigga" />
-          <img className='absolute bottom-0 z-10 w-full' src="/img/art.svg" alt="" />
-        </div>
-      </section>
-      <section className="flex flex-col gap-2 w-full bg-cream py-8">
-        <div className='px-4'>
-          <h1 className="font-thin font-serif uppercase text-lightDark md:text-5xl">Fullstack Web Developer</h1>
-          <h2 className='text-6xl leading-[54px] text-dark md:text-9xl md:leading-none'>Quentin <br /> Gibson</h2>
-        </div>
-        <div className="flex flex-col">
-          <div className='ml-16 border-l border-dark pl-4 font-serif'>
-            <p className='leading-6 text-lightDark max-w-[300px] my-4 text-base md:text-2xl'>Im a fullstack developer based in College Park, Ga and I enjoy playing with React. I love video games, basketball, and music.</p>
-            <div className="flex gap-3 text-lightDark">
-              <Link to="https://github.com/QuentinGibson">
-                <GrGithub className='text-4xl' />
-              </Link>
-              <Link to="https://twitter.com/quent_made_it">
-                <GrTwitter className='text-4xl' />
-              </Link>
-              <Link to="https://www.youtube.com/channel/UCsX8Ahu9O9dmFyoV_fgoeaw">
-                <GrYoutube className='text-4xl' />
-              </Link>
-            </div>
-            <div className="flex flex-col my-8 md:text-2xl">
-              <p className='font-thin  text-lightDark'>Born In</p>
-              <h3 className='font-bold '>College Park, Ga</h3>
-            </div>
-            <div className="flex flex-col my-8 md:text-2xl">
-              <p className='font-thin text-lightDark'>Experience</p>
-              <h3 className='font-bold '>10+ years</h3>
-            </div>
-            <div className="flex flex-col my-8 md:text-2xl">
-              <p className='font-thin text-lightDark'>Birthday</p>
-              <h3 className='font-bold '>November 29</h3>
+      <div className='grid md:grid-cols-2'>
+        <section className='py-8 bg-cream md:order-2'>
+          <div className='relative flex justify-center'>
+            <img src="/img/nobg-me.png" className='object-fill w-full z-20' alt="This is some random ass nigga" />
+            <img className='absolute bottom-0 z-10 w-full' src="/img/art.svg" alt="" />
+          </div>
+        </section>
+        <section className="flex flex-col gap-2 w-full bg-cream py-8">
+          <div className='px-4'>
+            <h1 className="font-thin font-serif uppercase text-lightDark md:text-5xl">Fullstack Web Developer</h1>
+            <h2 className='text-6xl leading-[54px] text-dark md:text-9xl md:leading-none'>Quentin <br /> Gibson</h2>
+          </div>
+          <div className="flex flex-col">
+            <div className='ml-16 border-l border-dark pl-4 font-serif'>
+              <p className='leading-6 text-lightDark max-w-[300px] my-4 text-base md:text-2xl'>Im a fullstack developer based in College Park, Ga and I enjoy playing with React. I love video games, basketball, and music.</p>
+              <div className="flex gap-3 text-lightDark">
+                <Link to="https://github.com/QuentinGibson">
+                  <GrGithub className='text-4xl' />
+                </Link>
+                <Link to="https://twitter.com/quent_made_it">
+                  <GrTwitter className='text-4xl' />
+                </Link>
+                <Link to="https://www.youtube.com/channel/UCsX8Ahu9O9dmFyoV_fgoeaw">
+                  <GrYoutube className='text-4xl' />
+                </Link>
+              </div>
+              <div className="flex flex-col my-8 md:text-2xl">
+                <p className='font-thin  text-lightDark'>Born In</p>
+                <h3 className='font-bold '>College Park, Ga</h3>
+              </div>
+              <div className="flex flex-col my-8 md:text-2xl">
+                <p className='font-thin text-lightDark'>Experience</p>
+                <h3 className='font-bold '>10+ years</h3>
+              </div>
+              <div className="flex flex-col my-8 md:text-2xl">
+                <p className='font-thin text-lightDark'>Birthday</p>
+                <h3 className='font-bold '>November 29</h3>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+
+      </div>
+
       <section className='py-8 bg-cream px-4'>
         <h1 className='text-5xl text-dark mb-4 md:text-8xl md:mb-8'>About Me</h1>
         <div className="grid grid-cols-2 gap-4 font-serif pb-4 md:grid-cols-4 md:pb-8">
@@ -73,20 +77,16 @@ export default function IndexRoute() {
         </div>
         <div className="flex flex-col font-serif text-lightDark leading-6 font-light pb-4">
           <p className='my-2'>
-            My name is Federico Ober. I have been studying UI UX Design since October 2020. I like creating a cool design project.
+            My name is Quentin Gibson, a passionate developer with an unwavering commitment to excellence. My journey in the world of programming began in 2014 and since then, I have been immersing myself in coding challenges. Every day I make sure to spend time creating a meaningful commit in order to continously get better. I have maintained this commitment even though no one has hired me.
           </p>
-          <p className="my-2">
-            Donec imperdiet risus at tortor consequat maximus et eget magna. Cras ornare sagittis augue, id sollicitudin justo tristique ut. Nullam ex enim, euismod vel bibendum ultrices, fringilla vel eros. Donec euismod leo lectus, et euismod metus euismod sed. Quisque quis suscipit ipsum, at pellentesque velit. Duis a congue sem.
-          </p>
-          <p className="my-2">
-            In sed dolor ut ligula malesuada sollicitudin. Morbi iaculis convallis arcu, nec maximus tellus sodales in. Curabitur blandit enim at nisl lobortis, non pretium enim hendrer.
-          </p>
+          <p className="my-2">As a developer, I believe that every line of code tells a story. I put emphasis on the clarity and maintainability of my code, ensuring that it can be easily understood by others. I make sure to follow famous programmers like celeberties in order to improve. How can you write better code if you dont read better code?</p>
+          <p className="my-2">I am a black developer from a infamous low income town. Most people never look my way when looking for a programmer, but I love doing this. I have been coding for years waiting to break into the field as its my dream job.</p>
         </div>
         <Link to="/contact" prefetch="intent" className='inline-block mt-8 px-8 py-3 font-bold font-serif border-2 border-[#ff8059] rounded-lg hover:bg-[#ff8059] hover:scale-105 transition-all duration-150'>
           Contact Me
         </Link>
         <div className='mt-8'>
-          <img className="" src="/img/signature.svg" alt="" />
+          <img className="max-w-[300px]" src="/img/signature.svg" alt="" />
         </div>
       </section>
       {/* <Canvas>
--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,6 +1,5 @@
 import { DataFunctionArgs, json, redirect } from "@remix-run/node";
-import { Link,  useFetcher } from "@remix-run/react";
-import { useEffect } from "react";
+import { Link, useFetcher } from "@remix-run/react";
 import { HiAtSymbol } from 'react-icons/hi'
 import invariant from "tiny-invariant";
 import { getSession, sessionStorage } from "~/session.server";
@@ -10,43 +9,43 @@ export default function ContactRoute() {
   return (
     <main className="bg-cream">
       <div className="max-w-screen-md mx-auto">
-      <h1 className="text-5xl text-dark  py-8 px-4">Contact</h1>
-      <div className="flex flex-col  font-serif py-8">
-        <div className="flex flex-col px-4">
-          <Link to="mailto:quentingibson94@gmail.com">
-            <HiAtSymbol className="text-4xl text-[#ff8059]" />
-            <p className="font-bold text-lg">quentingibson94@gmail.com</p>
-          </Link>
-        </div>
-      </div>
-      <section className="">
-        <contactFetcher.Form method="POST" className="font-serif">
-          <div className="flex flex-col px-4 gap-6">
-            <div className="flex flex-col">
-              <label htmlFor="name" className="font-bold">Name</label>
-              {contactFetcher.data && contactFetcher.data.error.name &&
-                <p className="text-red-500">{contactFetcher.data.error.name}</p>}
-              <input type="text" name="name" id="name" placeholder="Enter your name here" className="py-3 px-8 text-lightDark font-thin bg-cream border border-lightDark rounded mt-2" />
-            </div>
-            <div className="flex flex-col">
-              <label htmlFor="email" className="font-bold">Email Address</label>
-              {contactFetcher.data && contactFetcher.data.error.email &&
-                <p className="text-red-500">{contactFetcher.data.error.email}</p>}
-              <input type="email" name="email" id="email" placeholder="Enter your email address" className="py-3 px-8 text-lightDark font-thin bg-cream border border-lightDark rounded mt-2" />
-            </div>
-            <div className="flex flex-col">
-              <label htmlFor="message" className="font-bold">Message</label>
-              {contactFetcher.data && contactFetcher.data.error.message &&
-                <p className="text-red-500">{contactFetcher.data.error.message}</p>}
-              <textarea name="message" id="message" placeholder="Enter your message here" className="py-3 px-8 text-lightDark font-thin bg-cream border border-lightDark rounded mt-2 resize-y h-48" />
-            </div>
-            <div className="flex">
-              <button type="submit" className="px-6 py-2 border border-[#ff8059] rounded mb-8 hover:bg-[#ff8059]">Submit</button>
-            </div>
+        <h1 className="text-5xl text-dark  py-8 px-4">Contact</h1>
+        <div className="flex flex-col  font-serif py-8">
+          <div className="flex flex-col px-4">
+            <Link to="mailto:quentingibson94@gmail.com">
+              <HiAtSymbol className="text-4xl text-[#ff8059]" />
+              <p className="font-bold text-lg">quentingibson94@gmail.com</p>
+            </Link>
           </div>
-        </contactFetcher.Form>
-      </section>
-    
+        </div>
+        <section className="">
+          <contactFetcher.Form method="POST" className="font-serif">
+            <div className="flex flex-col px-4 gap-6">
+              <div className="flex flex-col">
+                <label htmlFor="name" className="font-bold">Name</label>
+                {contactFetcher.data && contactFetcher.data.error.name &&
+                  <p className="text-red-500">{contactFetcher.data.error.name}</p>}
+                <input type="text" name="name" id="name" placeholder="Enter your name here" className="py-3 px-8 text-lightDark font-thin bg-cream border border-lightDark rounded mt-2" />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="email" className="font-bold">Email Address</label>
+                {contactFetcher.data && contactFetcher.data.error.email &&
+                  <p className="text-red-500">{contactFetcher.data.error.email}</p>}
+                <input type="email" name="email" id="email" placeholder="Enter your email address" className="py-3 px-8 text-lightDark font-thin bg-cream border border-lightDark rounded mt-2" />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="message" className="font-bold">Message</label>
+                {contactFetcher.data && contactFetcher.data.error.message &&
+                  <p className="text-red-500">{contactFetcher.data.error.message}</p>}
+                <textarea name="message" id="message" placeholder="Enter your message here" className="py-3 px-8 text-lightDark font-thin bg-cream border border-lightDark rounded mt-2 resize-y h-48" />
+              </div>
+              <div className="flex">
+                <button type="submit" className="px-6 py-2 border border-[#ff8059] rounded mb-8 hover:bg-[#ff8059]">Submit</button>
+              </div>
+            </div>
+          </contactFetcher.Form>
+        </section>
+
       </div>
     </main>
   );
@@ -77,26 +76,26 @@ export const action = async ({ request, params }: DataFunctionArgs) => {
   }
 
   try {
-  invariant(typeof name === "string", "Name is invalid");
+    invariant(typeof name === "string", "Name is invalid");
   } catch (error: any) {
     return json({ error: { name: "Name is invalid. Please enter a valid name." } }, { status: 400, headers: { "Set-Cookie": await sessionStorage.commitSession(session) } })
   }
 
   try {
-  invariant(typeof email === "string", "Email is invalid");
-  invariant(email.includes("@"), "Email is invalid");
+    invariant(typeof email === "string", "Email is invalid");
+    invariant(email.includes("@"), "Email is invalid");
   } catch (error: any) {
     return json({ error: { email: "Email is invalid. Please enter a valid name." } }, { status: 400, headers: { "Set-Cookie": await sessionStorage.commitSession(session) } })
   }
 
   try {
-  invariant(name, "Name is required");
+    invariant(name, "Name is required");
   } catch (error: any) {
     return json({ error: { name: "Name is required." } }, { status: 400, headers: { "Set-Cookie": await sessionStorage.commitSession(session) } })
   }
 
   try {
-  invariant(email, "Email is required");
+    invariant(email, "Email is required");
   } catch (error: any) {
     return json({ error: { email: "Email is required." } }, { status: 400, headers: { "Set-Cookie": await sessionStorage.commitSession(session) } })
   }
@@ -108,7 +107,7 @@ export const action = async ({ request, params }: DataFunctionArgs) => {
   }
 
   try {
-  invariant(email.length < 100, "Email is too long");
+    invariant(email.length < 100, "Email is too long");
   } catch (error: any) {
     return json({ error: { email: "Email is too long." } }, { status: 400, headers: { "Set-Cookie": await sessionStorage.commitSession(session) } })
   }
